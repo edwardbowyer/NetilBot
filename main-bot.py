c83159b70d43bf82b480acb5b198c6a61c6f644e
edward.bowyer <edwardkbowyer@runbox.com>
2023-09-23 15:18:33 +0100

    Added streamer, notes on main

---
+++ main-bot.py
@@ -0,0 +1,5 @@
+- Load stream 
+- setup analysis loop (compare each chunk with the last to detect if song has changed, if it has, new song record created.)
+    > NetilBot is ignorant of tracklists, uses timestamps, processor of track id's will group into schedueele (human or robot) 
+    > Chunks need to be large enough to handle DJ mixes / blends. Or small enough? - Needs thought
+    > Write new track to external store. Format? Connectivity needs to be easy for MainWindowWrapper
---
+++ streamer/streamer.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+from pydub import AudioSegment
+from io import BytesIO
+
+STREAM_URI = "YOUR_AUDIO_STREAM_URI_HERE" # Need to parameterise/appsettings this
+CHUNK_SIZE = 4096  # Adjust based on the required buffer size should be config 
+
+def process_audio_chunk(chunk):
+    """Sample function to process the audio chunk."""
+    audio_data = BytesIO(chunk)
+    try:
+        audio = AudioSegment.from_mp3(audio_data)
+        samples = np.array(audio.get_array_of_samples())
+        
+        # For demonstration, plot the waveform of the chunk
+        plt.figure(figsize=(10, 4))
+        plt.plot(samples)
+        plt.title('Waveform of Chunk')
+        plt.xlabel('Sample')
+        plt.ylabel('Amplitude')
+        plt.show()
+        
+        # TODO: Add more analysis or processing as required
+
+    except Exception as e:
+        print(f"Error processing audio chunk: {e}")
+
+def stream_audio():
+    with requests.get(STREAM_URI, stream=True) as response:
+        response.raise_for_status()
+        audio_chunk = b""
+        for chunk in response.iter_content(CHUNK_SIZE):
+            audio_chunk += chunk
+            if len(audio_chunk) >= CHUNK_SIZE:
+                process_audio_chunk(audio_chunk)
+                audio_chunk = b""
+
+        # Process the last remaining chunk, if any
+        if audio_chunk:
+            process_audio_chunk(audio_chunk)
+
+if __name__ == "__main__":
+    stream_audio()
